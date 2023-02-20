5307ca07494837b15d81d10f8fced24c575d4b1c
Marcel Sands-McCalla <marcel.sands84@gmail.com>
2023-02-19 22:29:48 -0500

    adds auth0 controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/auth/logout' => 'auth0#logout'
 end
