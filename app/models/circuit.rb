95c129197190fbd054d5105b66fbdfad15aeab0c
Marcel Sands-McCalla <marcel.sands84@gmail.com>
2024-08-11 01:21:58 -0400

    refactor comments view and form

--- app/models/circuit.rb
+++ app/models/circuit.rb
@@ -1,5 +1,5 @@
 class Circuit < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :tracks, dependent: :destroy
   accepts_nested_attributes_for :tracks, allow_destroy: true
 
